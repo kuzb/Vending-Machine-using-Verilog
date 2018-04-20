963ee78c989661d48622b16e181e1d3bdd289911
burakkuz <burakkuz@sabanciuniv.edu>
2018-04-20 10:24:50 +0300

    Added project files

---
+++ vending_machine.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:39:09 12/30/2017 
+// Design Name: 
+// Module Name:    vending_machine 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module vending_machine(
+    );
+
+
+endmodule
